f8e30c4642c63d36266d397e5a4e45f0b6697ff6
GyuHyeong Nam <sjarbgud@gmail.com>
2023-08-11 23:38:35 +0900

    CGLIB - 예제 코드

---
+++ proxy/src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,24 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.service.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+
+    @Test
+    public void cglib() {
+        ConcreteService target = new ConcreteService();
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);  //CGLIB는 구체클래스를 상속받아서 프록시를 생성할 수 있다.
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+    }
+}
